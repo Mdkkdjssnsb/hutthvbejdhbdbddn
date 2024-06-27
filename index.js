7b9424886612ae974182d2466c817c00f5045114
Mdkkdjssnsb <163823860+Mdkkdjssnsb@users.noreply.github.com>
2024-06-27 13:05:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,16 +13,15 @@ app.get('/', (req, res) => {
   res.send('Welcome to TORF API Server');
 });
 
-// Function to get a random question
-const getRandomQuestion = (quizData) => {
-  const randomIndex = Math.floor(Math.random() * quizData.length);
-  return quizData[randomIndex];
+// Function to get a random item from an array
+const getRandomItem = (dataArray) => {
+  const randomIndex = Math.floor(Math.random() * dataArray.length);
+  return dataArray[randomIndex];
 };
 
 // TORF API endpoint to send a random question from quiz.json
 app.get('/api/torf', (req, res) => {
   try {
-    // Read quiz.json file
     const filePath = path.join(__dirname, 'quiz.json');
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
@@ -30,10 +30,27 @@ app.get('/api/torf', (req, res) => {
         return;
       }
       const quizData = JSON.parse(data);
-      // Get a random question
-      const randomQuestion = getRandomQuestion(quizData);
-      // Respond with the random question
+      const randomQuestion = getRandomItem(quizData);
       res.json(randomQuestion);
+    });
+  } catch (error) {
+    console.error('Unexpected error:', error);
+    res.status(500).json({ error: 'Unexpected error occurred' });
+  }
+});
+
+// Anime API endpoint to send anime data from anime.json
+app.get('/api/anime', (req, res) => {
+  try {
+    const filePath = path.join(__dirname, 'anime.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading anime data:', err);
+        res.status(500).json({ error: 'Failed to fetch anime data' });
+        return;
+      }
+      const animeData = JSON.parse(data);
+      res.json(animeData);
     });
   } catch (error) {
     console.error('Unexpected error:', error);
